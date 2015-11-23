978332733918884817dc2051585341653429c3fc
Phelipe de Sterlich <p.desterlich@gmail.com>
2015-11-23 11:46:49 +0100

    added modules
    - added modules async & mkdir
    - refactored repository list cleaning function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var path = require('path');
+var mkdirp = require('mkdirp');
 var commandLineArgs = require('command-line-args');
 
 var cli = commandLineArgs([
@@ -20,20 +22,27 @@ console.log("-----");
 
 var source = require('gitlab')({ url: options['source-url'], token: options['source-key'] });
 
-source.projects.all(function (list) {
-  list = list.map(function (item) {
+var cleanRepoList = function (list) {
+  return list.map(function (item) {
     return {
       group: item.namespace.name,
       name: item.name,
       description: item.description,
-      path: item.namespace.name + '/' + item.name
+      path: item.namespace.name + '/' + item.name,
+      url: item.ssh_url_to_repo
     };
   }).sort(function (a,b) {
     if (a.path < b.path) return -1;
     if (a.path > b.path) return 1;
     return 0;
   });
+};
+
+source.projects.all(function (list) {
+  list = cleanRepoList(list);
   list.forEach(function (project) {
-    console.log(project.path);
+    var fullPath = path.join(options['local-path'], project.group, project.name);
+    console.log(project.path + ' - ' + project.url);
+    mkdirp(fullPath);
   });
 });
